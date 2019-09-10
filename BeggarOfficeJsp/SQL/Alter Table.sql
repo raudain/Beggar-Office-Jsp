728673f010de9f6a2f8950573dce65410b617ba2
Roody <roody.audain@gmail.com>
2019-09-10 07:55:35 -0400

    update SQL

--- BeggarOfficeJsp/SQL/Alter Table.sql
+++ BeggarOfficeJsp/SQL/Alter Table.sql
@@ -4,9 +4,29 @@ DROP COLUMN cost;
 ALTER TABLE workers
 ADD cost int(11);
 
-ALTER TABLE Workers
-CHANGE Position Room
-VARCHAR(45);
+ALTER TABLE employees
+CHANGE Endurance endurance
+text;
 
-ALTER TABLE Workers
-MODIFY Cost bigint(11);
+ALTER TABLE employees
+MODIFY Level bit(5);
+
+/*
+	room
+*/
+ALTER TABLE employees
+MODIFY Room smallint(4);
+
+ALTER TABLE employees
+CHANGE Room room
+text;
+
+/*
+	name
+*/
+ALTER TABLE employees
+MODIFY Name smallint(4);
+
+ALTER TABLE employees
+CHANGE Name name
+text;

c1088a16cd3e33c5105c52d7335016640a318f36
roaudain <roody.audain@bmc.org>
2022-03-22 11:03:51 -0400

    Update Order by Cost.sql

--- SQL/Scripts/SQL Server/Order by Cost.sql
+++ SQL/Scripts/SQL Server/Order by Cost.sql
@@ -1,11 +1,11 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Room]
+SELECT [Room]
       ,[Name]
       ,[Profession]
       ,[Endurance]
       ,[Cost]
       ,[Cost with Comma]
   FROM [VirtualBeggar].[hr].[vw_workers]
-  WHERE [Cost] <> 0
+  WHERE 1=1
+ -- AND [Cost] <> 0
   AND [Profession] <> 'Businessman'
   ORDER BY [Cost]

5631039178a1bfe2491eeb158402bbbcf5a65192
roaudain <roody.audain@bmc.org>
2022-03-22 11:04:49 -0400

    Update Order by Cost.sql

--- SQL/Scripts/SQL Server/Order by Cost.sql
+++ SQL/Scripts/SQL Server/Order by Cost.sql
@@ -5,7 +5,7 @@ SELECT [Room]
       ,[Cost]
       ,[Cost with Comma]
   FROM [VirtualBeggar].[hr].[vw_workers]
-  WHERE 1=1
- -- AND [Cost] <> 0
+  WHERE 1 = 1
+  AND [Cost] <> 0
   AND [Profession] <> 'Businessman'
   ORDER BY [Cost]

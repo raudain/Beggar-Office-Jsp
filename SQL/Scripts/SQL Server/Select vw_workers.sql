a0c7b2c7095e683ebc572d1761cd8f6117472a10
roaudain <roody.audain@bmc.org>
2022-03-22 10:20:50 -0400

    Delete Select vw_workers.sql

--- SQL/Scripts/SQL Server/Select vw_workers.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [room] AS Room
-      ,[name] AS Name
-      ,[profession] AS Profession
-      ,[endurance] AS Endurance
-  FROM [VirtualBeggar].[hr].[vw_workers]

7a415c22fa9c8fb56da456b431e39c951553be7d
roaudain <roody.audain@bmc.org>
2022-03-29 10:08:48 -0400

    Delete outdated SQL files

--- SQL/Scripts/SQL Server/Select workers.sql
+++
@@ -1,6 +0,0 @@
-SELECT [room] AS [Room]
-      ,[name] AS [Name]
-      ,[profession_id] AS [Profession ID]
-      ,[endurance_id] AS [Endurance ID]
-      ,FORMAT(cost,'#,#') AS Cost
-  FROM [VirtualBeggar].[hr].[workers]

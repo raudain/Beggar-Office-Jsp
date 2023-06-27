25257143dbff1c139909458d0be8460cd9b65c0d
raudain <roody.audain@aol.com>
2023-06-27 13:14:34 -0400

    Corrected the workers
    Since I had the Apple Store made me erase my phone before repairing it and I forget to push my Virtual Beggar data to the cloud, my workers where reset to an earlier version. I had the manually enter the earlier version of my workers.

--- SQL/Scripts/Workers by Cost.sql
+++ SQL/Scripts/Workers by Cost.sql
@@ -2,5 +2,7 @@ SELECT WK.Room, WK.Name, PR.ProfessionName, EN.EnduranceName, WK.Cost
 FROM dbo.Worker WK
 INNER JOIN dbo.Profession PR ON PR.Profession = WK.Profession
 INNER JOIN Endurance EN ON EN.Endurance = WK.Endurance
-WHERE Cost != 0
-ORDER BY Cost;
+WHERE WK.Cost != 0
+AND PR.ProfessionName != "Businessman"
+ORDER BY WK.Cost;
+/*ORDER BY WK.Room;*/

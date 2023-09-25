00041cba289143d362df14f6cf991b52c931e388
raudain <roody.audain@aol.com>
2023-09-25 17:42:47 -0400

    uniform link class names

--- BeggarOfficeJsp/src/main/webapp/javascript/nav.js
+++ BeggarOfficeJsp/src/main/webapp/javascript/nav.js
@@ -4,15 +4,17 @@
 
 // Create Worker Button
 const rightButton = document.createElement("li");
-rightButton.className = "w3-bar-item w3-margin-right w3-right w3-mobile";
+rightButton.className = "w3-bar-item w3-hover-red w3-round-large " + 
+"w3-margin-right w3-right w3-mobile";
 const navigation = document.querySelector("ul");
 navigation.insertBefore(rightButton, null);
 const changeListButton = document.createElement("button");
 changeListButton.type = "button";
 if  (location.href == 'http://localhost:8080/BeggarOfficeJsp/Update')
-    changeListButton.className = "w3-button w3-yellow w3-hover-red w3-round-large";
+    changeListButton.className =
+    "w3-button w3-yellow w3-hover-red w3-round-large";
 else
-	changeListButton.className = "w3-button w3-hover-red w3-round-large";
+	changeListButton.className = "w3-button";
 rightButton.appendChild(changeListButton);
 const icon = document.createElement("i");
 icon.className = "fa-solid fa-keyboard w3-margin-right";
--- BeggarOfficeJsp/src/main/webapp/table.jsp
+++ BeggarOfficeJsp/src/main/webapp/table.jsp
@@ -40,11 +40,15 @@
 	
 	<script src = "javascript/nav.js"></script>
 	
-	<li class = "w3-hover-red w3-round-large w3-bar-item w3-margin-right w3-margin-left w3-mobile navigation">
+	<li class = "w3-bar-item w3-hover-red w3-round-large w3-margin-right w3-margin-left w3-mobile navigation">
 		<a href = ".">Tutorial</a>
 	</li>
-	<li class = "w3-yellow w3-hover-red w3-round-large w3-bar-item  w3-margin-right w3-mobile navigation">
+	<li class = "w3-bar-item w3-yellow w3-hover-red w3-round-large w3-margin-right w3-mobile navigation">
 		<a href = "Table?page=1">Workers Table</a>
+	</li>
+	<li class = "w3-bar-item w3-hover-red w3-round-large w3-margin-right w3-right w3-mobile navigation">
+		<a href = "Table?page=1">Export</a>
+		<i class="fa-solid fa-file-export"></i>
 	</li>
 	
 </ul>
